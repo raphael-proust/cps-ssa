340e3aee4c7cb9ee9065c579e150b749256bc1c4
Raphaël Proust <raphlalou@gmail.com>
2012-06-13 01:29:27 -0700

    [test] output result to appropriate files

---
+++ src/options.ml
@@ -0,0 +1,46 @@
+ (* {{{ LICENSE                                                              *
+  * vi: set fdm=marker fdl=0:                                                *
+  *                                                                          *
+  * Copyright (c) 2012 Raphaël Proust <raphlalou@gmail.com>                  *
+  * Copyright (c) 2012 INRIA - Raphaël Proust <raphlalou@gmail.com>          *
+  * Copyright (c) 2012 ENS - Raphaël Proust <raphlalou@gmail.com>            *
+  *                                                                          *
+  * Permission to use, copy, modify, and distribute this software for any    *
+  * purpose with or without fee is hereby granted, provided that the above   *
+  * copyright notice and this permission notice appear in all copies.        *
+  *                                                                          *
+  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES *
+  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF         *
+  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR  *
+  * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES   *
+  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN    *
+  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF  *
+  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.           *
+  * }}}                                                                      *)
+
+(* Default values *)
+
+let ll_files = ref []
+
+
+(*Parsing *)
+
+let arg_parsing = [
+  ]
+
+let add_ll_file s =
+  if Filename.check_suffix s ".ll" then
+    ll_files := s :: !ll_files
+  else
+    raise (Arg.Bad (s ^ " is not an .ll file"))
+
+let usage = "run.{byte,native} [options] file.ll [other_file.ll ..]"
+
+let () = Arg.parse arg_parsing add_ll_file usage
+
+
+(* Exporting dereferenced values *)
+
+let ll_files = !ll_files
+
+
--- src/test.ml
+++ src/test.ml
@@ -20,9 +20,11 @@
 
 let () = Printexc.record_backtrace true
 
-let run filename =
-  let in_chan = open_in filename            in
-  let lexbuf  = Lexing.from_channel in_chan in
+let run ll_file =
+
+  (* Phase 1: parse LLVM *)
+  let in_ll = open_in ll_file in
+  let lexbuf  = Lexing.from_channel in_ll in
   let llvm_prog =
     try
         Llvm_parser.module_ (Llvm_lexer.token) lexbuf
@@ -33,6 +35,9 @@ let run filename =
       Util.P.print_pos (Lexing.lexeme_end_p   lexbuf);
       raise e
   in
+  let () = close_in in_ll in
+
+  (* Phase 2: transform to SSA *)
   let ssa_prog =
     try
       LLVM2SSA.prog llvm_prog
@@ -42,7 +47,12 @@ let run filename =
       raise e
   in
   let ssa_doc = SSA_pp.pp_prog ssa_prog in
-  let () = Pprint.Channel.pretty 1. 20 stdout ssa_doc in
+  let ssa_file = Filename.chop_suffix ll_file ".ll" ^ ".ssa" in
+  let out_ssa = open_out ssa_file in
+  let () = Pprint.Channel.pretty 1. 20 out_ssa ssa_doc in
+  let () = close_out out_ssa in
+
+  (* Phase 3: transform to CPS *)
   let cps_m =
     try
       SSA2CPS.prog ssa_prog
@@ -52,11 +62,14 @@ let run filename =
       raise e
   in
   let cps_doc = CPS_pp.pp_m cps_m in
-  let () = Pprint.Channel.pretty 0. 100 stdout cps_doc in
+  let cps_file = Filename.chop_suffix ll_file ".ll" ^ ".cps" in
+  let out_cps = open_out cps_file in
+  let () = Pprint.Channel.pretty 0. 100 out_cps cps_doc in
+  let () = close_out out_cps in
   ()
 
 let () =
-    Array.iter
+    List.iter
       (fun t ->
         try
           run t
@@ -65,4 +78,4 @@ let () =
           Printf.eprintf "%s\n" (Printexc.to_string e);
           Printexc.print_backtrace stderr
       )
-      (Array.sub Sys.argv 1 (Array.length Sys.argv - 1))
+      Options.ll_files
